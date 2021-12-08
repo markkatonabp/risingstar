66e7944991fd6ab71478ca2b3e244b330c89f3ff
markkatonabp <markkatonabp@gmail.com>
2021-12-08 10:40:03 +0100

    Update webplayer-demo.js

--- content/webplayer-demo.js
+++ content/webplayer-demo.js
@@ -167,7 +167,7 @@ console.log( 'WebGL status: ' + webGLStatus );
 
 	// Set Camera
 	var camera = new PerspectiveCamera(50, container.offsetWidth / container.offsetHeight, 0.1, 100);
-	camera.position.set(0, 1.5, 0);
+	camera.position.set(0, 1.5, 5);
 	camera.far = 10000;
 	camera.near = 0.01;
 
